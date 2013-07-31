cdca0f3c3edf6ae88eebe0067640004d3499bc37
thradec <thradec@gmail.com>
2013-07-31 11:04:53 +0200

    upgrade wagon version to 2.1 and use apache http client implementation by default

--- src/main/java/org/jboss/wolf/validator/internal/InternalWagonProvider.java
+++ src/main/java/org/jboss/wolf/validator/internal/InternalWagonProvider.java
@@ -1,16 +1,44 @@
 package org.jboss.wolf.validator.internal;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.apache.maven.wagon.Wagon;
 import org.apache.maven.wagon.providers.file.FileWagon;
+import org.apache.maven.wagon.providers.http.HttpWagon;
 import org.apache.maven.wagon.providers.http.LightweightHttpWagon;
+import org.apache.maven.wagon.providers.http.LightweightHttpWagonAuthenticator;
+import org.apache.maven.wagon.providers.http.LightweightHttpsWagon;
 import org.eclipse.aether.connector.wagon.WagonProvider;
 
 public class InternalWagonProvider implements WagonProvider {
 
+    private final boolean useLightweightHttpWagon;
+
+    public InternalWagonProvider() {
+        this(false);
+    }
+
+    public InternalWagonProvider(boolean useLightweightHttpWagon) {
+        this.useLightweightHttpWagon = useLightweightHttpWagon;
+    }
+
     @Override
     public Wagon lookup(String roleHint) throws Exception {
-        if ("http".equals(roleHint) || "https".equals(roleHint)) {
-            return new LightweightHttpWagon();
+        if ("http".equals(roleHint)) {
+            if (useLightweightHttpWagon) {
+                LightweightHttpWagon lightweightHttpWagon = new LightweightHttpWagon();
+                FieldUtils.writeField(lightweightHttpWagon, "authenticator", new LightweightHttpWagonAuthenticator(), true);
+                return lightweightHttpWagon;
+            } else {
+                return new HttpWagon();
+            }
+        } else if ("https".equals(roleHint)) {
+            if (useLightweightHttpWagon) {
+                LightweightHttpsWagon lightweightHttpsWagon = new LightweightHttpsWagon();
+                FieldUtils.writeField(lightweightHttpsWagon, "authenticator", new LightweightHttpWagonAuthenticator(), true);
+                return lightweightHttpsWagon;
+            } else {
+                return new HttpWagon();
+            }
         } else if ("file".equals(roleHint)) {
             return new FileWagon();
         }
