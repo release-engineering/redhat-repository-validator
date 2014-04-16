295624b5e3251a693d7c62cc1ce384330cd66dff
Petr Siroky <psiroky@redhat.com>
2014-04-16 11:43:42 +0200

    Store additional metadata in BomDependencyNotFoundException
     * info about missing artifact and root dependency node can
       later be used in reporters and filters

--- src/main/java/org/jboss/wolf/validator/impl/bom/BomDependencyNotFoundException.java
+++ src/main/java/org/jboss/wolf/validator/impl/bom/BomDependencyNotFoundException.java
@@ -1,5 +1,7 @@
 package org.jboss.wolf.validator.impl.bom;
 
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.graph.DependencyNode;
 import org.eclipse.aether.resolution.DependencyResolutionException;
 
 public class BomDependencyNotFoundException extends Exception {
@@ -8,13 +10,27 @@ public class BomDependencyNotFoundException extends Exception {
 
     private final DependencyResolutionException dependencyResolutionException;
 
-    public BomDependencyNotFoundException(DependencyResolutionException dependencyResolutionException) {
+    private final Artifact missingArtifact;
+
+    private final DependencyNode dependencyNode;
+
+    public BomDependencyNotFoundException(DependencyResolutionException dependencyResolutionException, Artifact missingArtifact) {
         super(dependencyResolutionException);
         this.dependencyResolutionException = dependencyResolutionException;
+        this.missingArtifact = missingArtifact;
+        this.dependencyNode = dependencyResolutionException.getResult().getRoot();
     }
 
     public DependencyResolutionException getDependencyResolutionException() {
         return dependencyResolutionException;
+    }
+
+    public Artifact getMissingArtifact() {
+        return missingArtifact;
+    }
+
+    public DependencyNode getDependencyNode() {
+        return dependencyNode;
     }
 
 }
