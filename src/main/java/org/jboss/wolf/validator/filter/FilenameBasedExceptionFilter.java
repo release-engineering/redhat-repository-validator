7f27c76b8e5e00e0b8b24d0099d58184e4692bd9
Petr Siroky <psiroky@redhat.com>
2014-04-19 11:44:31 +0200

    Rename FileBasedExceptionFilter to FilenameBasedExceptionFilter
     * the new name better reflect the fact that the filter is
       operating on the file name not on the entire file

--- src/main/java/org/jboss/wolf/validator/filter/FilenameBasedExceptionFilter.java
+++ src/main/java/org/jboss/wolf/validator/filter/FilenameBasedExceptionFilter.java
@@ -5,12 +5,12 @@ import org.jboss.wolf.validator.ExceptionFilter;
 import java.io.File;
 import java.util.regex.Pattern;
 
-public class FileBasedExceptionFilter implements ExceptionFilter {
+public class FilenameBasedExceptionFilter implements ExceptionFilter {
     private final Class<? extends Exception> exceptionClass;
 
     private final Pattern filenamePattern;
 
-    public FileBasedExceptionFilter(Class<? extends Exception> exceptionClass, String filenameRegex) {
+    public FilenameBasedExceptionFilter(Class<? extends Exception> exceptionClass, String filenameRegex) {
         this.exceptionClass = exceptionClass;
         this.filenamePattern = Pattern.compile(filenameRegex);
     }
