65239b1eeed17e8da9dfa9333ada92d4284b70ca
Martin Swiech <mswiech@redhat.com>
2015-01-16 19:43:09 +0100

    [filter file] added and implemented filter "file".

---
+++ src/test/java/org/jboss/wolf/validator/filter/TestFilenameBasedExceptionFilter.java
@@ -0,0 +1,34 @@
+package org.jboss.wolf.validator.filter;
+
+import org.jboss.wolf.validator.impl.signature.JarSignatureVerificationException;
+import org.junit.Test;
+
+import java.io.File;
+
+public class TestFilenameBasedExceptionFilter extends AbstractExceptionFilterTest {
+
+    @Test
+    public void shouldIgnoreMatchingFileAndMatchingException() {
+        FilenameBasedExceptionFilter filter = new FilenameBasedExceptionFilter("filename.*", JarSignatureVerificationException.class);
+        Exception ex = new JarSignatureVerificationException(new File("filename"), "some message");
+        assertExceptionIgnored(filter, ex, new File("filename"));
+        // simple test to verify the regex: "filename.*" will also capture the string "filename2"
+        ex = new JarSignatureVerificationException(new File("filename2"), "Some message");
+        assertExceptionIgnored(filter, ex, new File("filename2"));
+    }
+
+    @Test
+    public void shouldNotIgnoreOnlyMatchingFile() {
+        FilenameBasedExceptionFilter filter = new FilenameBasedExceptionFilter("filename.*", JarSignatureVerificationException.class);
+        Exception ex = new RuntimeException("Some message");
+        assertExceptionNotIgnored(filter, ex, new File("filename"));
+    }
+
+    @Test
+    public void shouldNotIgnoreOnlyMatchingException() {
+        FilenameBasedExceptionFilter filter = new FilenameBasedExceptionFilter("filename.*", JarSignatureVerificationException.class);
+        Exception ex = new JarSignatureVerificationException(new File("filename"), "Some message");
+        assertExceptionNotIgnored(filter, ex, new File("foo"));
+    }
+
+}
